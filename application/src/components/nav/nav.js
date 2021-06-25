67bad97ca042c7ded550522cda6d425877b46d2c
Matthew Spach <matthewspach@gmail.com>
2021-06-25 09:04:58 -0700

    feature(logout): logout clears token and email

--- application/src/components/nav/nav.js
+++ application/src/components/nav/nav.js
@@ -1,27 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./nav.css";
+import { connect } from 'react-redux';
+import { logoutUser } from '../../redux/actions/authActions';
 
-const Nav = (props) => {
-    return (
-        <div className="nav-strip">
-            <Link to={"/order"} className="nav-link">
-                <div className="nav-link-style">
-                    <label className="nav-label">Order Form</label>
-                </div>
-            </Link>
-            <Link to={"/view-orders"} className="nav-link" id="middle-link">
-                <div className="nav-link-style">
-                    <label className="nav-label">View Orders</label>
-                </div>
-            </Link>
-            <Link to={"/login"} className="nav-link">
-                <div className="nav-link-style">
-                    <label className="nav-label">Log Out</label>
-                </div>
-            </Link>
+const mapActionsToProps = dispatch => ({
+  logout() { dispatch(logoutUser()) }
+})
+
+const Nav = (props) =>{
+  return (
+    <div className="nav-strip">
+      <Link to={"/order"} className="nav-link">
+        <div className="nav-link-style">
+            <label className="nav-label">Order Form</label>
         </div>
-    );
+      </Link>
+      <Link to={"/view-orders"} className="nav-link" id="middle-link">
+        <div className="nav-link-style">
+            <label className="nav-label">View Orders</label>
+        </div>
+      </Link>
+      <Link to={"/"} className="nav-link" onClick={() => props.logout()}>
+        <div className="nav-link-style">
+            <label className="nav-label">Log Out</label>
+        </div>
+      </Link>
+    </div>
+  );
 }
 
-export default Nav;
+export default connect(null, mapActionsToProps)(Nav);
