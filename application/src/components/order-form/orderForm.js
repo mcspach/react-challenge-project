dbebd89e3401546e1d060a3346ba1a7e2c3c0c5e
Matthew Spach <matthewspach@gmail.com>
2021-06-29 11:28:48 -0700

    feature(edit&delete): implement frontend functions for edit/delete order

--- application/src/components/order-form/orderForm.js
+++ application/src/components/order-form/orderForm.js
@@ -4,10 +4,12 @@ import { connect } from 'react-redux';
 import { SERVER_IP } from '../../private';
 import './orderForm.css';
 
-const ADD_ORDER_URL = `${SERVER_IP}/api/add-order`
+const ADD_ORDER_URL = `${SERVER_IP}/api/add-order`;
+const EDIT_ORDER_URL = `${SERVER_IP}/api/edit-order`;
 
 const mapStateToProps = (state) => ({
     auth: state.auth,
+    order_id: state.id
 })
 
 class OrderForm extends Component {
@@ -20,30 +22,60 @@ class OrderForm extends Component {
     }
 
     menuItemChosen(event) {
-        this.setState({ item: event.target.value });
+        this.setState({ order_item: event.target.value });
     }
 
     menuQuantityChosen(event) {
         this.setState({ quantity: event.target.value });
     }
 
-    submitOrder(event) {
+    async submitOrder(event) {
         event.preventDefault();
         if (this.state.order_item === "") return;
-        fetch(ADD_ORDER_URL, {
-            method: 'POST',
-            body: JSON.stringify({
-                order_item: this.state.order_item,
-                quantity: this.state.quantity,
-                ordered_by: this.props.auth.email || 'Unknown!',
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        .then(res => res.json())
-        .then(response => console.log("Success", JSON.stringify(response)))
-        .catch(error => console.error(error));
+        console.log(this.state.order_id);
+        if (this.state.order_id) {
+            await fetch(EDIT_ORDER_URL, {
+                method: 'POST',
+                body: JSON.stringify({
+                    id: this.state.order_id,
+                    order_item: this.state.order_item,
+                    quantity: this.state.quantity,
+                    ordered_by: this.props.auth.email || 'Unknown!',
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(res => res.json())
+            .then(response => console.log("Edit Success", JSON.stringify(response)))
+            .catch(error => console.error(error)); 
+        } else {
+            await fetch(ADD_ORDER_URL, {
+                method: 'POST',
+                body: JSON.stringify({
+                    order_item: this.state.order_item,
+                    quantity: this.state.quantity,
+                    ordered_by: this.props.auth.email || 'Unknown!',
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(res => res.json())
+            .then(response => console.log("Success", JSON.stringify(response)))
+            .catch(error => console.error(error));
+            }   
+        this.props.history.push('/view-orders');
+    }
+
+    componentDidMount() {
+        if (this.props.location.state) {
+          this.setState({
+            order_id: this.props.location.state.id,
+            order_item: this.props.location.state.order_item,
+            quantity: this.props.location.state.quantity,
+          });
+        }
     }
 
     render() {
@@ -81,3 +113,27 @@ class OrderForm extends Component {
 }
 
 export default connect(mapStateToProps, null)(OrderForm);
+
+
+// PseudoCodeed steps for issue #15
+
+// This pull request will include changes from ORDER ITEM and USER EMAIL in order to test the app
+// Use PostMan to test API calls if needed
+
+// Edit:
+// -Add onClick function to edit button. - directs to edit form and passes order_id
+// -Update the orderForm component to render as 'edit order' if user passes an id
+// -Just set state when the component renders, then use conditional to render EDIT or ADD based on whether or not there is an ID in state
+// -Create update function by calling API in similar form as NEW order
+// -push to view-orders page with history
+
+// Delete:
+// -Add event listener to button that takes order id
+// -Button will call API to delete that record
+// -At the end call the fetch method again to force rerendering on the page
+// -Switch componentDidMount code into a function called getOrders()
+
+// 
+// Delete Order (POST) - (server addr)/api/delete-order (Expects id in request body)
+// Edit Order (POST) - (server addr)/api/edit-order (Expects id in request body. Will look for ordered_by, quantity, menu_item.)
+// Flush Orders (DELETE) - (server addr)/api/delete-all (This deletes all current orders in the DB)
